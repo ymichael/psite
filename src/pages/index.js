a24ee7d22ec4f0797fe15cd18cf6115632decbf3
Michael Yong <wrong92@gmail.com>
2018-03-08 20:57:32 -0800

    Test out client side rendering the home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = ({ data }) => (
-  <div>
-    <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-  </div>
-)
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { markdownRemark: { html: '' } }
+  }
+
+  componentWillMount() {
+    // Run a test to see how Google indexes this page.
+    if (typeof window != 'undefined' && window.document) {
+      this.setState({ markdownRemark: this.props.data.markdownRemark })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          dangerouslySetInnerHTML={{ __html: this.state.markdownRemark.html }}
+        />
+      </div>
+    )
+  }
+}
 
 export default IndexPage
 
